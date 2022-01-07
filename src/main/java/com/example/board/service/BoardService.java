d65057016503ffe3bf2e8923912a4894fb5c2729
jaeminLee524 <rkwoals524@hanmail.net>
2022-01-07 16:24:04 +0900

    service

---
+++ src/main/java/com/example/board/service/BoardService.java
@@ -0,0 +1,12 @@
+package com.example.board.service;
+
+import com.example.board.dto.BoardDto;
+
+import java.util.List;
+
+public interface BoardService {
+
+    List<BoardDto> selectBoardList() throws Exception;
+
+    void insertBoard(BoardDto boardDto) throws Exception;
+}
